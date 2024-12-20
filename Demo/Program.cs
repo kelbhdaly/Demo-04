a7cd34de7c7f8f618f0262d3bf7689ff2ce87ecb
khaled elbhdaly <155319819+kelbhdaly@users.noreply.github.com>
2024-12-20 06:20:38 +0200

    Function Def

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,7 +1,50 @@
 ﻿namespace Demo
 {
+
+    //Class ->Function
+    //Instructor -> Function
+    //Interface -> Function
+    //enum
     internal class Program
     {
+
+
+        //static void PrintShap()
+        // {
+        //     for (int i = 0; i < 10; i++)
+        //     {
+        //         Console.WriteLine("^-^");
+        //     }
+        // }
+
+        //static void PrintShap(int count)
+        //{
+        //    for (int i = 0; i < count; i++)
+        //    {
+        //        Console.WriteLine("^-^");
+        //    }
+        //}
+        //static void PrintShap(int count, string Pattern)
+        //{
+        //    for (int i = 0; i < count; i++)
+        //    {
+        //        Console.WriteLine(Pattern);
+        //    }
+        //}
+
+        //static void PrintShap(int count , string Pattern="*-*")
+        //{
+        //    for (int i = 0; i < count; i++)
+        //    {
+        //        Console.WriteLine(Pattern);
+        //    }
+        //}
+
+       static int SumNumbers(int Number01 , int Number02)
+        {
+            return Number01 + Number02;
+        }
+        //Entery point
         static void Main(string[] args)
         {
             #region One D Array
@@ -102,30 +145,49 @@
             //Marks[1] = new int[] { 2, 6 };
             //Marks[2] = new int[] { 20, 30, 40, 50 };
 
-            int[][] Marks = new int[3][];
-            Marks[0] = new int[3];
-            Marks[1]= new int[1];
-            Marks[2]= new int[5];
+            #region Two D Juddged
+            //int[][] Marks = new int[3][];
+            //Marks[0] = new int[3];
+            //Marks[1]= new int[1];
+            //Marks[2]= new int[5];
 
-            for (int i = 0; i < Marks.Length; i++) 
-            {
-                for(int j = 0; j < Marks[i].Length; j++)
-                {
-                    Console.Write($"Enter the number {i} {j} : ");
-                    Marks[i][j] = int.Parse(Console.ReadLine());
-                }
-            }
+            //for (int i = 0; i < Marks.Length; i++) 
+            //{
+            //    for(int j = 0; j < Marks[i].Length; j++)
+            //    {
+            //        Console.Write($"Enter the number {i} {j} : ");
+            //        Marks[i][j] = int.Parse(Console.ReadLine());
+            //    }
+            //}
 
-            for (int i = 0; i < Marks.Length; i++)
-            {
-                for (int j = 0; j < Marks[i].Length; j++)
-                {
-                    Console.Write($" {Marks[i] [j]} : ");
-                    
-                }
-                Console.WriteLine();
+            //for (int i = 0; i < Marks.Length; i++)
+            //{
+            //    for (int j = 0; j < Marks[i].Length; j++)
+            //    {
+            //        Console.Write($" {Marks[i] [j]} : ");
 
-            }
+            //    }
+            //    Console.WriteLine(); 
+
+
+            //}
+            #endregion
+
+            // Function : Block of code 
+
+            //Function Prototype
+            // 1-  Signature
+            // 1-1 Name
+            // 1-2 Return Type
+            // 1-3 Parameters(Input)
+            // 2-  Body (Code)
+
+            //calling For The Functions
+            //PrintShap(5 , "khaled"); //Passing By Order
+           // PrintShap(Pattern:"ayhaga",count: 5); //Passing By Name
+           //var sum =SumNumbers(1, 2);
+           // Console.WriteLine(sum);
+           // Console.WriteLine(SumNumbers(2,3));
         }
     }
 }
